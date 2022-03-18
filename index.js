5af6eb809c659530d76e4861d6e13f90291bb2d0
Maria Marquez <406rem33@gmail.com>
2022-03-18 14:37:53 -0700

    Completes assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+  cats.push(name); 
+};
+
+ function destructivelyPrependCat(name){
+   cats.unshift(name);
+ };
+
+ function destructivelyRemoveLastCat(){
+   cats.pop();
+};
+
+function destructivelyRemoveFirstCat(){
+  cats.shift();
+};
+
+function appendCat(name){
+  const apCat = cats.slice();
+  apCat.push(name);
+return apCat;
+};
+
+function prependCat(name){
+   const preCat = cats.slice();
+   preCat.unshift(name);
+   return preCat;
+};
+
+function removeLastCat(){
+  const remCat = cats.slice();
+  remCat.pop();
+  return remCat;
+};
+
+function removeFirstCat(){
+  const firCat = cats.slice();
+  firCat.shift();
+  return firCat
+};
